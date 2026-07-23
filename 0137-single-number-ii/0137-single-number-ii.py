9c044b7faac9b62c29818273f1f35c16665eb1c7
manvitha209 <manvithamanu209@gmail.com>
2026-07-23 23:01:01 +0530

    Time: 8 ms (35.01%), Space: 20.6 MB (44.97%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        rtype: int
+        one=0
+        two=0
+        for i in range(len(nums)):
+            one=(one^nums[i])&~two
+            two=(two^nums[i])&~one
+        return one
